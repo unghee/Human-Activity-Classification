70dcf1cb4cd16fd88bca779ac0f88ee765be5f08
JordLee <unghee@umich.edu>
2019-12-14 18:15:11 +0900

    forloop clean up

--- train_nway.py
+++ train_nway.py
@@ -21,12 +21,8 @@ from networks import Network
 ########## SETTINGS  ########################
 
 ## for N way classifieres
-numb_class1 = 6
-numb_class2 = 2
-numb_class3 = 2
-numb_class4 = 2
-numb_class5 = 2
-numb_class6 = 2
+numb_class= [6,2,2,2,2,2]
+
 
 # numb_class = 6
 
@@ -47,56 +43,29 @@ def save_object(obj, filename):
 # Load the dataset and train, val, test splits
 print("Loading datasets...")
 
-## calling for the first time
-# BIO_train= EnableDataset(subject_list= ['156','185','186','188','189','190', '191', '192', '193', '194'],data_range=(1,48),window_size=500,processed=True)
-# BIO_val= EnableDataset(subject_list= ['156','185','186','189','190', '191', '192', '193', '194'],data_range=(48,49),window_size=500,processed=True)
-# BIO_test= EnableDataset(subject_list= ['156','185','189','190', '192', '193', '194'],data_range=(49,50),window_size=500,processed=True)
-
 BIO_trains=[]
 BIO_vals=[]
 BIO_tests=[]
-for i in range(1,7):
-    BIO_trains.append(EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True,label=i))
-    BIO_vals.append(EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=i))
-    BIO_tests.append(EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=i))
+for i in range(6):
+    BIO_trains.append(EnableDataset(subject_list= ['156','185','186','188','189','190', '191', '192', '193', '194'],data_range=(1,48),window_size=500,processed=True,label=i+1))
+    BIO_vals.append(EnableDataset(subject_list= ['156','185','186','188','189','190', '191', '192', '193', '194'],data_range=(48,49),window_size=500,processed=True,label=i+1))
+    BIO_tests.append(EnableDataset(subject_list= ['156','185','186','188','189','190', '191', '192', '193', '194'],data_range=(49,50),window_size=500,processed=True,label=i+1))
     
-
-# BIO_train1= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True,label=1)
-# BIO_train2= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True, label=2)
-# BIO_train3= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True, label=3)
-# BIO_train4= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True, label=4)
-# BIO_train5= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True, label=5)
-# BIO_train6= EnableDataset(subject_list= ['156','185'],data_range=(1,8),window_size=500,processed=True, label=6)
-
-# BIO_val1= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=1)
-# BIO_val2= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=2)
-# BIO_val3= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=3)
-# BIO_val4= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=4)
-# BIO_val5= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=5)
-# BIO_val6= EnableDataset(subject_list= ['156'],data_range=(40,48),window_size=500,processed=True,label=6)
-
-
-# BIO_test1= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=1)
-# BIO_test2= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=2)
-# BIO_test3= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=3)
-# BIO_test4= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=4)
-# BIO_test5= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=5)
-# BIO_test6= EnableDataset(subject_list= ['156'],data_range=(42,50),window_size=500,processed=True,label=6)
 
 
 # ## saving dataset a file
 
 save_object(BIO_trains, 'BIO_trains.pkl')
-save_object(BIO_vals, 'BIO_vals')
-save_object(BIO_tests, 'BIO_tests')
+save_object(BIO_vals, 'BIO_vals.pkl')
+save_object(BIO_tests, 'BIO_tests.pkl')
 
 # ## load from saved files
-# with open('BIO_train_.pkl', 'rb') as input:
-#     BIO_train = pickle.load(input)
-# with open('BIO_val_.pkl', 'rb') as input:
-#     BIO_val = pickle.load(input)
-# with open('BIO_test_.pkl', 'rb') as input:
-#     BIO_test = pickle.load(input)
+# with open('BIO_trains.pkl', 'rb') as input:
+#     BIO_trains = pickle.load(input)
+# with open('BIO_vals.pkl', 'rb') as input:
+#     BIO_vals = pickle.load(input)
+# with open('BIO_tests.pkl', 'rb') as input:
+#     BIO_tests = pickle.load(input)
 
 
 ## check the class distribution
@@ -119,207 +88,63 @@ def weight_classes(dataset):
     sum_classes = np.sum(classes)
     for idx in classes:
         if idx != 0 :
-            weights.append(idx)
+            # weights.append(sum_classes/idx)
+            weights.append(1)
         else:
             continue
-            # weights.append(0)
-    # weights = [sum_classes/idx  for idx in classes if idx]
     print(weights)
 
-    # idx =0
-    # weight_samples = [0] * len(dataset)  
-    # for val, target in dataset:
-    #         # target= target.numpy()
-    #         # target = target[0]
-    #         # pdb.set_trace()
-    #         target = int(target)
-    #         # pdb.set_trace()
-    #         weight_samples[idx] = weights[target-1]  
-    #         idx +=1
-    # print('weigted_samples',len(weighted_samples))
-    # print('length of data',len(dataset))
 
     weights = torch.FloatTensor(weights)
     
 
     return weights
+
 weights_list=[]
-for i in range(1,7):
-    weights_list.append(BIO_train[i])
-# weights1 = weight_classes(BIO_train1)
-# weights2 = weight_classes(BIO_train2)
-# weights3 = weight_classes(BIO_train3)
-# weights4 = weight_classes(BIO_train4)
-# weights5 = weight_classes(BIO_train5)
-# weights6 = weight_classes(BIO_train6)
+for i in range(6):
+    weights_list.append(weight_classes(BIO_trains[i]))
 
-
-
-
-# weight_samples1 = weight_classes(BIO_train1)
-# weight_samples2 = weight_classes(BIO_train2)
-# weight_samples3 = weight_classes(BIO_train3)
-# weight_samples4 = weight_classes(BIO_train4)
-# weight_samples5 = weight_classes(BIO_train5)
-# weight_samples6 = weight_classes(BIO_train6)
-
-# weight_samples1= torch.DoubleTensor(weight_samples1)   
-# sampler1 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples1, len(weight_samples1)) 
-# weight_samples2= torch.DoubleTensor(weight_samples2)   
-# sampler2 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples2, len(weight_samples2)) 
-# weight_samples1= torch.DoubleTensor(weight_samples3)   
-# sampler3 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples3, len(weight_samples3)) 
-# weight_samples1= torch.DoubleTensor(weight_samples4)   
-# sampler4 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples4, len(weight_samples4)) 
-# weight_samples1= torch.DoubleTensor(weight_samples5)   
-# sampler5 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples5, len(weight_samples5)) 
-# weight_samples1= torch.DoubleTensor(weight_samples6)   
-# sampler6 = torch.utils.data.sampler.WeightedRandomSampler(weight_samples6, len(weight_samples6)) 
-
-
-# # trainloader = DataLoader(BIO_train, shuffle=False, sampler=sampler,batch_size=BATCH_SIZE)
-
-
-# trainloader1 = DataLoader(BIO_train1, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler1)
-# trainloader2 = DataLoader(BIO_train2, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler2)
-# trainloader3 = DataLoader(BIO_train3, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler3)
-# trainloader4 = DataLoader(BIO_train4, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler4)
-# trainloader5 = DataLoader(BIO_train5, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler5)
-# trainloader6 = DataLoader(BIO_train6, shuffle=False,batch_size=BATCH_SIZE,sampler=sampler6)
-
-trainloader1 = DataLoader(BIO_train1, shuffle=False,batch_size=BATCH_SIZE)
-trainloader2 = DataLoader(BIO_train2, shuffle=False,batch_size=BATCH_SIZE)
-trainloader3 = DataLoader(BIO_train3, shuffle=False,batch_size=BATCH_SIZE)
-trainloader4 = DataLoader(BIO_train4, shuffle=False,batch_size=BATCH_SIZE)
-trainloader5 = DataLoader(BIO_train5, shuffle=False,batch_size=BATCH_SIZE)
-trainloader6 = DataLoader(BIO_train6, shuffle=False,batch_size=BATCH_SIZE)
-
-valloader1 = DataLoader(BIO_val1, shuffle=False,batch_size=BATCH_SIZE)
-valloader2 = DataLoader(BIO_val2, shuffle=False,batch_size=BATCH_SIZE)
-valloader3 = DataLoader(BIO_val3, shuffle=False,batch_size=BATCH_SIZE)
-valloader4 = DataLoader(BIO_val4, shuffle=False,batch_size=BATCH_SIZE)
-valloader5 = DataLoader(BIO_val5, shuffle=False,batch_size=BATCH_SIZE)
-valloader6 = DataLoader(BIO_val6, shuffle=False,batch_size=BATCH_SIZE)
-
-testloader1 = DataLoader(BIO_test1, shuffle=False,batch_size=BATCH_SIZE)
-testloader2 = DataLoader(BIO_test2, shuffle=False,batch_size=BATCH_SIZE)
-testloader3 = DataLoader(BIO_test3, shuffle=False,batch_size=BATCH_SIZE)
-testloader4 = DataLoader(BIO_test4, shuffle=False,batch_size=BATCH_SIZE)
-testloader5 = DataLoader(BIO_test5, shuffle=False,batch_size=BATCH_SIZE)
-testloader6 = DataLoader(BIO_test6, shuffle=False,batch_size=BATCH_SIZE)
-
-
-## with no sampler
-# trainloader = DataLoader(BIO_train, batch_size=32, shuffle=True)
-# valloader = DataLoader(BIO_val, batch_size=32,shuffle=True)
-# testloader = DataLoader(BIO_test, batch_size=32,shuffle=True)
+trainloaders=[]
+valloaders=[]
+testloaders=[]
+for i in range(6):
+    trainloaders.append(DataLoader(BIO_trains[i], shuffle=False,batch_size=BATCH_SIZE))
+    valloaders.append(DataLoader(BIO_vals[i], shuffle=False,batch_size=BATCH_SIZE))
+    testloaders.append(DataLoader(BIO_tests[i], shuffle=False,batch_size=BATCH_SIZE))
 
 
 device = "cuda" if torch.cuda.is_available() else "cpu" # Configure device
 print('GPU USED?',torch.cuda.is_available())
-# model = Network().to(device)
+
+################# MODELS#####################
+
+models =[]
+optimizers =[]
+criterions =[]
+
+for i in range(6):
+    # # model = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
+    # # num_ftrs = model.fc.in_features
+    # # model.fc = nn.Linear(num_ftrs, numb_class1)
+    model = Network(output_dim=numb_class[i])
+    model = model.to(device)
+    model.eval()
+    models.append(model)
+    weights_list[i] = weights_list[i].to(device)
+    criterions.append(nn.CrossEntropyLoss(weight=weights_list[i])) # Specify the loss layer
+    # TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
+    optimizers.append(optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
+    del model
 
 
-################# MODEL1#####################
-# model = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model.fc.in_features
-# model.fc = nn.Linear(num_ftrs, numb_class1)
-
-model = Network(output_dim=numb_class1)
-
-model = model.to(device)
-model.eval()
-
-weights1 = weights1.to(device)
-criterion1 = nn.CrossEntropyLoss(weight=weights_list[0]) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer1 = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-
-################# MODEL2#####################
-# model2 = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model2.fc.in_features
-# model2.fc = nn.Linear(num_ftrs, numb_class2)
-
-model2 = Network(output_dim=numb_class2)
-
-model2 = model2.to(device)
-model2.eval()
-weights2 = weights2.to(device)
-criterion2 = nn.CrossEntropyLoss(weight=weights2) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer2 = optim.Adam(model2.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-
-################# MODEL3#####################
-# model3 = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model3.fc.in_features
-# model3.fc = nn.Linear(num_ftrs, numb_class3)
-
-
-model3 = Network(output_dim=numb_class3)
-
-model3 = model3.to(device)
-model3.eval()
-weights3 = weights3.to(device)
-criterion3 = nn.CrossEntropyLoss(weight=weights3) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer3 = optim.Adam(model3.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-
-################# MODEL4#####################
-# model4 = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model4.fc.in_features
-# model4.fc = nn.Linear(num_ftrs, numb_class4)
-
-model4 = Network(output_dim=numb_class4)
-
-model4 = model4.to(device)
-model4.eval()
-weights4 = weights4.to(device)
-criterion4 = nn.CrossEntropyLoss(weight=weights4) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer4 = optim.Adam(model4.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-################# MODEL5#####################
-# model5 = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model5.fc.in_features
-# model5.fc = nn.Linear(num_ftrs, numb_class5)
-
-model5 = Network(output_dim=numb_class5)
-
-model5 = model5.to(device)
-model5.eval()
-weights5 = weights5.to(device)
-criterion5 = nn.CrossEntropyLoss(weight=weights5) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer5 = optim.Adam(model5.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-
-################# MODEL6#####################
-# model6 = torch.hub.load('pytorch/vision:v0.4.2', 'resnext50_32x4d', pretrained=True) # use resnet
-# num_ftrs = model6.fc.in_features
-# model6.fc = nn.Linear(num_ftrs, numb_class6)
-
-
-model6 = Network(output_dim=numb_class6)
-
-model6 = model5.to(device)
-model6.eval()
-weights6 = weights6.to(device)
-criterion6 = nn.CrossEntropyLoss(weight=weights6) # Specify the loss layer
-# TODO: Modify the line below, experiment with different optimizers and parameters (such as learning rate)
-optimizer6 = optim.Adam(model6.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) # Specify optimizer and assign trainable parameters to it, weight_decay is L2 regularization strength
-
-
-            ####
-            # 1 : LW, 2:RA, 3:RD, 4:SA, 5:SD, 6:Stand
-            # MODEL1: LW(1)-> LW(1), RA(2), RD(3), SA(4), SD(5), Stand(6) 
-            # MODEL2: RA(2)-> LW(1), RA(2)
-            # MODEL3: RD(3)-> LW(1), RD(3)
-            # MODEL4: SA(4)-> LW(1), SA(4)
-            # MODEL5: SD(5)-> LW(1), SD(5)
-            # MODEL6: Stand(6)-> LW(1), SD(6)
+#### MODE-SPECIFIC CLASSIFIER SCHEME
+# 1 : LW, 2:RA, 3:RD, 4:SA, 5:SD, 6:Stand
+# MODEL1: LW(1)-> LW(1), RA(2), RD(3), SA(4), SD(5), Stand(6) 
+# MODEL2: RA(2)-> LW(1), RA(2)
+# MODEL3: RD(3)-> LW(1), RD(3)
+# MODEL4: SA(4)-> LW(1), SA(4)
+# MODEL5: SD(5)-> LW(1), SD(5)
+# MODEL6: Stand(6)-> LW(1), SD(6)
 
 def train(model, criterion, optimizer, loader, valloader, num_epoch = 20,label_no=None): # Train the model
     loss_history=[]
@@ -374,24 +199,32 @@ def evaluate(model, loader,label_no): # Evaluate accuracy on validation / test s
         print('empty dataset!!', label_no)
     return correct, len(loader.dataset), acc
 
-loss_history, val_history =train(model, criterion1, optimizer1, trainloader1,valloader1, num_epoch,label_no=1)
-loss_history2, val_history2 =train(model2, criterion2, optimizer2, trainloader2,valloader2, num_epoch,label_no=2)
-loss_history3, val_history3 =train(model3, criterion3, optimizer3, trainloader3, valloader3,num_epoch,label_no=3)
-loss_history4, val_history4 =train(model4, criterion4, optimizer4, trainloader4, valloader4,num_epoch,label_no=4)
-loss_history5, val_history5 =train(model5, criterion5, optimizer5, trainloader5, valloader5,num_epoch,label_no=5)
-loss_history6, val_history6 =train(model6, criterion6, optimizer6, trainloader6, valloader6,num_epoch,label_no=6)
+loss_historys=[]
+val_historys=[]
+for i in range(6):
+    loss_history, val_history =train(models[i], criterions[i], optimizers[i], trainloaders[i], valloaders[i], num_epoch, label_no=i+1)
+    loss_historys.append(loss_history)
+    val_historys.append(val_history)
 
 
 print("Evaluate on test set")
-corr1, len_data1,_ =evaluate(model, testloader1,label_no=1)
-corr2, len_data2,_ =evaluate(model2, testloader2,label_no=2)
-corr3, len_data3,_ =evaluate(model3, testloader3,label_no=3)
-corr4, len_data4,_ =evaluate(model4, testloader4,label_no=4)
-corr5, len_data5,_ =evaluate(model5, testloader5,label_no=5)
-corr6, len_data6,_ =evaluate(model6, testloader6,label_no=6)
 
-corr_total = corr1+corr2+corr3+corr4+corr5+corr6
-len_data_total= len_data1+len_data2+len_data3+len_data4+len_data5+len_data6
+
+corrs=[]
+len_datas=[]
+for i in range(6):
+    corr, len_data,_ =evaluate(models[i], testloaders[i],label_no=i+1)
+    corrs.append(corr)
+    len_datas.append(len_data)
+
+
+corr_total =0
+len_data_total=0
+for i in range(6):
+    corr_total = corrs[i] + corr_total
+    len_data_total = len_data_total + len_datas[i]
+
+
 acc_total = corr_total/len_data_total
 print("Total Evaluation accuracy: {}".format(acc_total))
 
