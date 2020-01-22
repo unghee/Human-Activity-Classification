95fc6b6e1e33cf1374dfba2dd93528212db6b8e6
Rishi Patel <patelris@umich.edu>
2020-01-22 11:07:28 -0500

    Added 36 output network for predicting (current, future) transition pairs

---
+++ 36-output/dataset.py
@@ -0,0 +1,206 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+import cv2
+from scipy import signal, stats
+import matplotlib
+from matplotlib import pyplot as plt
+
+from tqdm import tqdm # Displays a progress bar
+
+import pandas as pd
+import numpy as np
+
+import os
+import torchvision.transforms.functional as F
+
+
+import librosa
+
+import librosa.display
+
+
+class EnableDataset(Dataset):
+    '''
+    dataDir: path to folder containing data
+    subject_list: the subjects to be included in dataset
+    data_range: the specified circuit trials for each subject
+    window_size: how many samples to consider for a label
+    time_series: when true, use time_series method
+    label: when specified, the dataset will only contain data with the given label value
+    transform: optional transform to apply to the data
+    '''
+    def __init__(self, dataDir='./Data/' ,subject_list=['156'], data_range=(1, 10), window_size=500, time_series=False, label=None, transform=None,bands=None,hop_length=None):
+
+        print("    range: [%d, %d)" % (data_range[0], data_range[1]))
+        self.dataset = []
+        self.prev_label = np.array([], dtype=np.int64)
+        self.img_data_stack=np.empty((51, 3, 4, 51), dtype=np.int64)
+        self.transform = transform
+
+        for subjects in subject_list:
+            for i in range(data_range[0], data_range[1]):
+                filename = dataDir +'AB' + subjects+'/Processed/'+'AB' + subjects+ '_Circuit_%03d_post.csv'% i
+                print(filename)
+                if not os.path.exists(filename):
+                    print(filename, 'not found')
+                    continue
+                raw_data = pd.read_csv(filename)
+
+                segmented_data = np.array([], dtype=np.int64).reshape(0,window_size,48)
+                labels = np.array([], dtype=np.int64)
+                timesteps = []
+                triggers = []
+                index = 0
+
+                gait_events = ['Right_Heel_Contact','Right_Toe_Off','Left_Heel_Contact','Left_Toe_Off']
+                for event in gait_events:
+                    print(event)
+                    while not pd.isnull(raw_data.loc[index, event]):
+                        trigger = raw_data.loc[index, event+'_Trigger']
+                        trigger=str(int(trigger))
+                        # if label is None or float(trigger[0]) == label or float(trigger[2]) != 6  and float(trigger[0]) !=6:
+                        if float(trigger[2]) != 6 and float(trigger[0]) !=6:
+                            timesteps.append(raw_data.loc[index, event])
+                            trigger = raw_data.loc[index, event+'_Trigger']
+                            trigger=str(int(trigger))
+                            triggers.append(trigger) # triggers can be used to compare translational and steady-state error
+                            prev = float(trigger[0])
+                            current = float(trigger[1])
+                            labels = np.append(labels, [prev*6 + current], axis=0)
+                            print('label:', prev*6 + current)
+                            if float(trigger[2]) == 6:
+                                print('***********',trigger[2])
+                            self.prev_label = np.append(self.prev_label,[float(trigger[0])], axis =0)
+                        index += 1
+                    index = 0
+
+                for idx,timestep in enumerate(timesteps):
+                    if timestep-window_size-1 >= 0:
+                        data = np.array(raw_data.loc[timestep-window_size-1:timestep-2, 'Right_Shank_Ax':'Left_Knee_Velocity'])
+                        if not time_series:
+                            # img= self.spectrogram2(data)/128.0-1.0
+                            img= self.melspectrogram(data,bands=bands ,hop_length=hop_length)
+                            # plt.imshow(img)
+                            # plt.show()
+
+                            self.dataset.append((img,labels[idx]))
+                        else:
+                            self.dataset.append((data.T,labels[idx]))
+        print("load dataset done")
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        img, label = self.dataset[index]
+        if self.transform:
+            img = F.to_pil_image(np.uint8(img))
+            img = self.transform(img)
+            img = np.array(img)
+        return torch.FloatTensor(img), torch.LongTensor(np.array(label) )
+
+    def spectrogram2(self, segmented_data, fs=500,hamming_windowsize=30, overlap = 15):
+        vals = []
+        for i in range(0,17):
+	        for x in range(3*i,3*(i+1)):
+	            row = segmented_data[:,x]
+	            f, t, Sxx = signal.spectrogram(row, fs, window=signal.windows.hamming(hamming_windowsize, True), noverlap=5)
+	            tmp, _ = stats.boxcox(Sxx.reshape(-1,1))
+	            Sxx = tmp.reshape(Sxx.shape)-np.min(tmp)
+	            Sxx = Sxx/np.max(Sxx)*255
+	            vals.append(Sxx)
+        # the way of stacking should be fixed
+        out = np.stack(vals, axis=0)
+        # out=out.astype(np.uint8)
+        return out
+
+    def melspectrogram(self, segmented_data, fs=500,bands=64 ,hop_length=50):
+
+        ###### STACKING UP MULTIPLE SPECTOGRAM APPROACH!
+
+        vals = []
+        # vals2 =[]
+        for i in range(0,17):
+            for x in range(3*i,3*(i+1)):
+                row = segmented_data[:,x]
+                melspec_full = librosa.feature.melspectrogram(y=row,sr=fs,n_fft=hop_length*2, hop_length=hop_length,n_mels=bands)
+                logspec_full = librosa.amplitude_to_db(melspec_full)
+                logspec_delta = librosa.feature.delta(logspec_full) # add derivative
+                # librosa.display.specshow(logspec_full, x_axis='time',y_axis='mel', sr=fs,fmax=fs/2)
+                # plt.colorbar(format='%+02.0f dB')
+                # plt.imshow(logspec_full)
+                # plt.show()
+                # plt.close()
+
+                vals.append(logspec_full)
+                # vals2.append(logspec_delta)
+        # out = np.stack(vals, axis=0)
+        # out = out.astype(np.uint8)
+
+        # features = np.concatenate((vals,vals2),axis=2)
+
+        # out = np.asarray(out).reshape(len(out),bands,frames, 1)
+
+        return vals
+
+
+    def cwt(self, segmented_data, fs=500,hamming_windowsize=30, overlap = 15):
+        vals = []
+        for i in range(0,17):
+            for x in range(3*i,3*(i+1)):
+                row = segmented_data[:,x]
+                widths = np.arange(1,101)
+                cwtmatr = signal.cwt(row, signal.ricker, widths)
+                print(cwtmatr.shape, np.min(cwtmatr), np.max(cwtmatr))
+                cwtmatr = cwtmatr-np.min(cwtmatr)
+                cwtmatr = cwtmatr/np.max(cwtmatr)*255
+                vals.append(cwtmatr)
+
+
+        out = np.stack(vals, axis=0)
+        out=out.astype(np.uint8)
+        return out
+
+
+    def spectrogram(self, segmented_data, fs=500, hamming_windowsize=10):
+        vals1 = []
+        for x in range(3):
+            row = segmented_data[y,:,x]
+            f, t, Sxx = signal.spectrogram(row, fs, window=signal.windows.hamming(100, True), noverlap=50)
+            fig = plt.figure()
+            ax = fig.add_axes([0.,0.,1.,1.])
+            fig.set_size_inches((5,5))
+            ax.pcolormesh(t, f, Sxx, cmap='gray')
+            ax.axis('off')
+            fig.add_axes(ax)
+            fig.canvas.draw()
+            # this rasterized the figure
+            X = np.array(fig.canvas.renderer._renderer)
+            X = 0.2989*X[:,1] + 0.5870*X[:,2] + 0.1140*X[:,3]
+            vals1.append(X)
+            plt.close()
+        vals2 = []
+        for x in range(6,9):
+            row = segmented_data[y,:,x]
+            f, t, Sxx = signal.spectrogram(row, fs, window=signal.windows.hamming(100, True), noverlap=50)
+            fig = plt.figure()
+            ax = fig.add_axes([0.,0.,1.,1.])
+            fig.set_size_inches((5,5))
+            ax.pcolormesh(t, f, Sxx, cmap='gray')
+            ax.axis('off')
+            fig.add_axes(ax)
+            fig.canvas.draw()
+            # this rasterized the figure
+            X = np.array(fig.canvas.renderer._renderer)
+            X = 0.2989*X[:,1] + 0.5870*X[:,2] + 0.1140*X[:,3]
+            vals2.append(X)
+            plt.close()
+
+        out1 = np.stack(vals1, axis=2).astype(np.uint8)
+        out2 = np.stack(vals2, axis=2).astype(np.uint8)
+        out = np.hstack((out1, out2))
+        cv2.imshow("ret", out)
+        cv2.waitKey(0)
+        return ret
---
+++ 36-output/train.py
@@ -0,0 +1,177 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm # Displays a progress bar
+
+import torch
+from torch import nn
+from torch import optim
+import torch.nn.functional as F
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset, Subset, DataLoader, random_split
+
+from dataset import EnableDataset
+
+import pickle
+
+
+
+
+########## SETTINGS  ########################
+
+BATCH_SIZE = 32
+LEARNING_RATE = 1e-5
+WEIGHT_DECAY = 1e-3
+NUMB_CLASS = 36
+NUB_EPOCH=60
+############################################
+
+
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.sclayer1 = nn.Sequential(
+            nn.Conv2d(51, 128, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.sclayer2 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            )
+        self.drop_out = nn.Dropout()
+        self.fc1 = nn.Linear( 4096, 2000)
+        # self.fc1 = nn.Linear( 8192, 2000)
+        # self.fc1 = nn.Linear( 20480, 2000)
+        self.fc2 = nn.Linear(2000, NUMB_CLASS)
+
+
+    def forward(self,x):
+        x = self.sclayer1(x)
+        x = self.sclayer2(x)
+        x = x.reshape(x.size(0), -1)
+        x = self.drop_out(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+
+        return x
+# Load the dataset and train, val, test splits
+print("Loading datasets...")
+
+
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
+
+def weight_classes(dataset):
+    trainloader = DataLoader(dataset, shuffle=False,batch_size=BATCH_SIZE)
+    classes = [0] * 36
+    for data, labels in trainloader:
+        for x in range(labels.size()[0]):
+            classes[labels[x]] +=1
+
+
+    ## with sample
+    weights=[]
+    sum_classes = np.sum(classes)
+    for idx in classes:
+        if idx != 0 :
+            weights.append(sum_classes/idx)
+        else:
+            weights.append(0)
+
+    print(weights)
+    weights = torch.FloatTensor(weights)
+
+
+    return weights
+
+
+BIO_train= EnableDataset(subject_list= ['156'],data_range=(1, 15),bands=16,hop_length=27)
+# BIO_train= EnableDataset(subject_list= ['156'],data_range=(1, 5),bands=16,hop_length=27)
+
+save_object(BIO_train,'BIO_train_melspectro_reduced.pkl')
+
+# with open('BIO_train_melspectro_5label.pkl', 'rb') as input:
+    # BIO_train = pickle.load(input)
+
+
+train_size = int(0.8 * len(BIO_train))+1
+test_size = int((len(BIO_train) - train_size)/2)
+print(len(BIO_train), train_size, test_size)
+train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(BIO_train, [train_size, test_size, len(BIO_train) - train_size - test_size])
+# Create dataloaders
+trainloader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+# classes = [0,0,0,0,0,0,0]
+# for data, labels in trainloader:
+#     for x in range(labels.size()[0]):
+#         classes[labels[x]] +=1
+# print(classes)
+valloader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
+testloader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
+
+
+# numb_class = 5
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu" # Configure device
+print('GPU USED?',torch.cuda.is_available())
+model = Network()
+model = model.to(device)
+
+# weights = weight_classes(BIO_train)
+
+# weights = torch.FloatTensor([0.0, 1.0, 9693/2609, 9693/3250, 9693/1181, 9693/1133, 9693/530 ])
+# weights = weights.to(device)
+# criterion = nn.CrossEntropyLoss(weight=weights)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+num_epoch = NUB_EPOCH
+
+def train(model, loader, num_epoch = 20): # Train the model
+    loss_history=[]
+    val_history=[]
+    print("Start training...")
+    model.train()
+    for i in range(num_epoch):
+        running_loss = []
+        for batch, label in tqdm(loader):
+            batch = batch.to(device)
+            label = label.to(device)
+            optimizer.zero_grad()
+            pred = model(batch)
+            loss = criterion(pred, label)
+            running_loss.append(loss.item())
+            loss.backward()
+            optimizer.step()
+            loss_history.append(np.mean(running_loss))
+        val_acc = evaluate(model, valloader)
+        val_history.append(val_acc)
+        print("Epoch {} loss:{} val_acc:{}".format(i+1,np.mean(running_loss),val_acc))
+    print("Done!")
+    return loss_history, val_history
+
+def evaluate(model, loader):
+    model.eval()
+    correct = 0
+    # labels = [0,0,0,0,0,0,0]
+    # totalcount = [0,0,0,0,0,0,0]
+    with torch.no_grad():
+        count = 0
+        totalloss = 0
+        for batch, label in tqdm(loader):
+            batch = batch.to(device)
+            label = label.to(device)
+            pred = model(batch)
+            totalloss += criterion(pred, label)
+            count +=1
+            correct += (torch.argmax(pred,dim=1) % 6 == label% 6).sum().item()
+    acc = correct/len(loader.dataset)
+    print("Evaluation loss: {}".format(totalloss/count))
+    print("Evaluation accuracy: {}".format(acc))
+    return acc
+
+loss_history, val_history =train(model, trainloader, num_epoch)
+print("Evaluate on validation set...")
+evaluate(model, valloader)
+print("Evaluate on test set")
+evaluate(model, testloader)
