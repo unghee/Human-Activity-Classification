e8eca67e04e2b9d8559cb0c8f1baa6e6a6caf700
Rishi Patel <patelris@umich.edu>
2020-01-29 01:14:53 -0500

    Made the dataset.py consistent for Freq-Encoding and LDA Added Steady state and Translational to LDA

--- Freq-Encoding/dataset.py
+++ Freq-Encoding/dataset.py
@@ -26,62 +26,129 @@ class EnableDataset(Dataset):
     subject_list: the subjects to be included in dataset
     data_range: the specified circuit trials for each subject
     window_size: how many samples to consider for a label
-    time_series: when true, use time_series method
-    label: when specified, the dataset will only contain data with the given label value
     transform: optional transform to apply to the data
     '''
-    def __init__(self, dataDir='./Data/' ,subject_list=['156'], data_range=(1, 10), window_size=500, time_series=False, label=None, transform=None,bands=None,hop_length=None):
+    def __init__(self, dataDir='../Data/', subject_list=['156'], model_type="CNN", data_range=(1, 10), window_size=500,  sensors=["imu","emg", "goin"], mode="bilateral", transform=None,bands=None,hop_length=None,phaselabel=None,prevlabel=None,delay=0):
+        self.model_type = model_type
+        if self.model_type == "CNN":
+            print("    range: [%d, %d)" % (data_range[0], data_range[1]))
+            self.dataset = []
+            self.prev_label = np.array([], dtype=np.int64)
+            self.img_data_stack=np.empty((51, 3, 4, 51), dtype=np.int64)
+            self.transform = transform
 
-        print("    range: [%d, %d)" % (data_range[0], data_range[1]))
-        self.dataset = []
-        self.prev_label = np.array([], dtype=np.int64)
-        self.img_data_stack=np.empty((51, 3, 4, 51), dtype=np.int64)
-        self.transform = transform
+            for subjects in subject_list:
+                for i in range(data_range[0], data_range[1]):
+                    filename = dataDir +'AB' + subjects+'/Processed/'+'AB' + subjects+ '_Circuit_%03d_post.csv'% i
+                    if not os.path.exists(filename):
+                        print(filename, 'not found')
+                        continue
+                    raw_data = pd.read_csv(filename)
 
-        for subjects in subject_list:
-            for i in range(data_range[0], data_range[1]):
-                filename = dataDir +'AB' + subjects+'/Processed/'+'AB' + subjects+ '_Circuit_%03d_post.csv'% i
-                if not os.path.exists(filename):
-                    print(filename, 'not found')
-                    continue
-                raw_data = pd.read_csv(filename)
+                    segmented_data = np.array([], dtype=np.int64).reshape(0,window_size,48)
+                    labels = np.array([], dtype=np.int64)
+                    timestep_type = []
+                    timesteps = []
+                    triggers = []
+                    index = 0
+                    gait_event_types = []
 
-                segmented_data = np.array([], dtype=np.int64).reshape(0,window_size,48)
-                labels = np.array([], dtype=np.int64)
-                timesteps = []
-                triggers = []
-                index = 0
-
-                gait_events = ['Right_Heel_Contact','Right_Toe_Off','Left_Heel_Contact','Left_Toe_Off']
-                for event in gait_events:
-                    while not pd.isnull(raw_data.loc[index, event]):
-                        trigger = raw_data.loc[index, event+'_Trigger']
-                        trigger=str(int(trigger))
-                        # if label is None or float(trigger[0]) == label or float(trigger[2]) != 6  and float(trigger[0]) !=6:
-                        if float(trigger[2]) != 6 and float(trigger[0]) !=6 and float(trigger[0]) !=0 and float(trigger[2]) !=0:
-                            timesteps.append(raw_data.loc[index, event])
+                    gait_events = ['Right_Heel_Contact','Right_Toe_Off','Left_Heel_Contact','Left_Toe_Off']
+                    for event in gait_events:
+                        while not pd.isnull(raw_data.loc[index, event]):
                             trigger = raw_data.loc[index, event+'_Trigger']
                             trigger=str(int(trigger))
-                            triggers.append(trigger) # triggers can be used to compare translational and steady-state error
-                            labels = np.append(labels,[float(trigger[2])], axis =0)
-                            if float(trigger[2]) == 6:
-                                print('***********',trigger[2])
-                            self.prev_label = np.append(self.prev_label,[float(trigger[0])], axis =0)
-                        index += 1
+                            if float(trigger[2]) != 6 and float(trigger[0]) !=6:
+                                timesteps.append(raw_data.loc[index, event])
+                                trigger = raw_data.loc[index, event+'_Trigger']
+                                trigger=str(int(trigger))
+                                triggers.append(trigger) # triggers can be used to compare translational and steady-state error
+
+                                labels = np.append(labels,[float(trigger[2])], axis =0)
+
+                                if trigger[0] == trigger[2]:
+                                    timestep_type.append(1)
+                                else:
+                                    timestep_type.append(0)
+
+                                if "right" in event.lower():
+                                    gait_event_types.append("Right")
+                                else:
+                                    gait_event_types.append("Left")
+
+                                self.prev_label = np.append(self.prev_label,[float(trigger[0])], axis =0)
+                            index += 1
+                        index = 0
+
+                    for idx,timestep in enumerate(timesteps):
+                        data = raw_data.loc[timestep-window_size-1:timestep-2,:]
+                        if timestep-window_size-1 >= 0:
+                            if mode == "ipsilateral":
+                                data = data.filter(regex='(?=.*'+ gait_event_types[idx] + '|Mode|Waist)(?!.*Toe)(?!.*Heel)(.+)', axis=1)
+                            elif mode == "contralateral":
+                                opposite = "Left" if gait_event_types[idx] == "Right" else "Right"
+                                data = data.filter(regex='(?=.*'+ opposite + '|Mode|Waist)(?!.*Toe)(?!.*Heel)(.+)', axis=1)
+                            else:
+                                data = data.filter(regex="^((?!Heel|Toe).)*$", axis=1)
+
+                            regex = "(?=Mode|.*Ankle.*|.*Knee.*"
+                            if "imu" in sensors:
+                                regex += "|.*A[xyz].*"
+                            if "goin" in sensors:
+                                regex += "|.*G[xyz].*"
+                            if "emg" in sensors:
+                                regex += "|.*TA.*|.*MG.*|.*SOL.*|.*BF.*|.*ST.*|.*VL.*|.*RF.*"
+                            regex += ")"
+                            data = data.filter(regex=regex, axis=1)
+
+                            data = np.array(data)
+                            img= self.melspectrogram(data,bands=bands ,hop_length=hop_length)
+                            self.dataset.append((img,labels[idx], timestep_type[idx]))
+        else:
+            self.dataset = []
+            self.prev_label = np.array([], dtype=np.int64)
+
+            for subjects in subject_list:
+                    filename = dataDir +'AB' + subjects+'/Features/'+'AB' + subjects+ '_Features_'+ str(300-delay) + '.csv'
+                    if not os.path.exists(filename):
+                        print(filename, 'not found')
+                        continue
+                    raw_data = pd.read_csv(filename)
+
+                    timesteps = []
+                    timestep_type = []
+                    triggers = []
                     index = 0
 
-                for idx,timestep in enumerate(timesteps):
-                    if timestep-window_size-1 >= 0:
-                        data = np.array(raw_data.loc[timestep-window_size-1:timestep-2, 'Right_Shank_Ax':'Left_Knee_Velocity'])
-                        if not time_series:
-                            # img= self.spectrogram2(data)/128.0-1.0
-                            img= self.melspectrogram(data,bands=bands ,hop_length=hop_length)
-                            # plt.imshow(img)
-                            # plt.show()
 
-                            self.dataset.append((img,labels[idx]))
+                    # while not pd.isnull(raw_data.loc[index,'Trigger']):
+                    for index in range(0,raw_data.shape[0]):
+                        trigger = raw_data.loc[index,'Trigger']
+                        trigger=str(int(trigger))
+                        phase = raw_data.loc[index,'Leg Phase']
+                        if trigger[0] == trigger[2]:
+                            timestep_type.append(1)
                         else:
-                            self.dataset.append((data.T,labels[idx]))
+                            timestep_type.append(0)
+                        if prevlabel is not None:
+                            if float(phase) == phaselabel and float(trigger[0]) == prevlabel and float(trigger[2]) != 6 and float(trigger[0]) !=6:
+                                triggers.append(trigger)
+                                label = float(trigger[2])
+                                if float(trigger[2]) == 6:
+                                    print('***********',trigger[2])
+                                data = np.array(raw_data.loc[index, :'Contra RF AR6'])
+                                self.dataset.append((data.T,label, timestep_type[-1]))
+                        else:
+                            if float(trigger[2]) != 6 and float(trigger[0]) !=6:
+
+                                triggers.append(trigger) # triggers can be used to compare translational and steady-state error
+
+                                label = float(trigger[2])
+                                if float(trigger[2]) == 6:
+                                    print('***********',trigger[2])
+
+                                data = np.array(raw_data.loc[index, :'Contra RF AR6'])
+                                self.dataset.append((data.T,label, timestep_type[-1]))
         print("load dataset done")
 
 
@@ -89,12 +156,16 @@ class EnableDataset(Dataset):
         return len(self.dataset)
 
     def __getitem__(self, index):
-        img, label = self.dataset[index]
-        if self.transform:
-            img = F.to_pil_image(np.uint8(img))
-            img = self.transform(img)
-            img = np.array(img)
-        return torch.FloatTensor(img), torch.LongTensor(np.array(label) )
+        if self.model_type == "CNN":
+            img, label, timestep_type = self.dataset[index]
+            if self.transform:
+                img = F.to_pil_image(np.uint8(img))
+                img = self.transform(img)
+                img = np.array(img)
+            return torch.FloatTensor(img), torch.LongTensor(np.array(label)), timestep_type
+        else:
+            img, label, timestep_type = self.dataset[index]
+            return img, np.array(label), timestep_type
 
     def spectrogram2(self, segmented_data, fs=500,hamming_windowsize=30, overlap = 15):
         vals = []
@@ -106,38 +177,22 @@ class EnableDataset(Dataset):
 	            Sxx = tmp.reshape(Sxx.shape)-np.min(tmp)
 	            Sxx = Sxx/np.max(Sxx)*255
 	            vals.append(Sxx)
-        # the way of stacking should be fixed
         out = np.stack(vals, axis=0)
-        # out=out.astype(np.uint8)
         return out
 
     def melspectrogram(self, segmented_data, fs=500,bands=64 ,hop_length=50):
 
-        ###### STACKING UP MULTIPLE SPECTOGRAM APPROACH! 
+        ###### STACKING UP MULTIPLE SPECTOGRAM APPROACH!
 
         vals = []
-        # vals2 =[]
         for i in range(0,17):
             for x in range(3*i,3*(i+1)):
                 row = segmented_data[:,x]
-                melspec_full = librosa.feature.melspectrogram(y=row,sr=fs,n_fft=hop_length*2, hop_length=hop_length,n_mels=bands) 
-                logspec_full = librosa.amplitude_to_db(melspec_full) 
+                melspec_full = librosa.feature.melspectrogram(y=row,sr=fs,n_fft=hop_length*2, hop_length=hop_length,n_mels=bands)
+                logspec_full = librosa.amplitude_to_db(melspec_full)
                 logspec_delta = librosa.feature.delta(logspec_full) # add derivative
-                # librosa.display.specshow(logspec_full, x_axis='time',y_axis='mel', sr=fs,fmax=fs/2)
-                # plt.colorbar(format='%+02.0f dB') 
-                # plt.imshow(logspec_full)
-                # plt.show()
-                # plt.close()
-  
+
                 vals.append(logspec_full)
-                # vals2.append(logspec_delta)
-        # out = np.stack(vals, axis=0)
-        # out = out.astype(np.uint8)
-
-        # features = np.concatenate((vals,vals2),axis=2)
-
-        # out = np.asarray(out).reshape(len(out),bands,frames, 1) 
-
         return vals
 
 
