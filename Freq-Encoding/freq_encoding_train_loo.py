08d18a474cd31a82f004253b5e42397d5b9266b7
Rishi Patel <patelris@umich.edu>
2020-02-17 12:05:46 -0500

    Tried to reduce memory in Freq Encoding. Also commented out debugging statements in dataset.py

--- Freq-Encoding/freq_encoding_train_loo.py
+++ Freq-Encoding/freq_encoding_train_loo.py
@@ -118,13 +118,17 @@ def run_classifier(mode='bilateral',classifier='CNN',sensor=["imu","emg","goin"]
 			X_train = batch
 			y_train = label
 			types_train = dtype
+		BIO_train = None
+		train_set = None
 
 		BIO_test = torch.utils.data.ConcatDataset(test_set)
-		wholeloader = DataLoader(BIO_test, batch_size=len(BIO_train))
+		wholeloader = DataLoader(BIO_test, batch_size=len(BIO_test))
 		for batch, label, dtype in tqdm(wholeloader,disable=DATA_LOAD_BOOL):
 			X_test = batch
 			y_test = label
 			types_test = dtype
+		BIO_test = None
+		test_set = None
 
 		train_dataset = TensorDataset( X_train, y_train, types_train)
 		test_dataset = TensorDataset( X_test, y_test, types_test)
@@ -144,7 +148,7 @@ def run_classifier(mode='bilateral',classifier='CNN',sensor=["imu","emg","goin"]
 		preds.extend(pred)
 		tests.extend(test)
 
-		for i in range(len(class_acc)):
+		for i in range(len(class_accs)):
 			class_accs[i] += class_acc[i]
 
 		i +=1
